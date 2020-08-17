370f2380a8c79c9e89e35a4d7e19ea02c000bbd0
jawish-a@outlook.com <jawish-a@outlook.com>
2020-08-17 10:16:17 +0300

    working code with if inside while loop

--- cashier.py
+++ cashier.py
@@ -1,5 +1,24 @@
 def main():
 	# write code here
+	items = []
+	while True:
+		item_name = input("Item (enter \"done\" when finished): ")
+		if item_name == "done":
+			break
+		item_price = int(input("Price: "))
+		item_qty = int(input("Quantity: "))
+		items.append({"name":item_name, "price": item_price, "quantity": item_qty})
+
+	print("-------------------")
+	print("receipt")
+	print("-------------------")
+	total = 0
+	for item in items:
+		print(str(item["quantity"]) + item["name"] + str(item["price"]*item["quantity"]) + "KD" )
+		total += item["price"]*item["quantity"]
+	print("-------------------")
+	print("Total Price: " + str(total) + "KD")
+
 
 if __name__ == '__main__':
 	main()
